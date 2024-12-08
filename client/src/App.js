0f0ee205b403ff1eee2081088048030b1329b1c9
muawwaznc <muawwaznc@gmail.com>
2024-12-08 16:53:11 +0500

    Add routes of all the pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 
 // Pages Link
 import Index from "./pages/index";
+import About from "./pages/about";
 import Contact from "./pages/contact";
+import Doctors from "./pages/doctors";
+import Chatbot from "./pages/chatbot";
+import Treatment from "./pages/treatment";
+
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Index />} />
-                <Route path="/Contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/doctors" element={<Doctors />} />
+                <Route path="/chatbot" element={<Chatbot />} />
+                <Route path="/treatment" element={<Treatment />} />
             </Routes>
         </div>
     );
